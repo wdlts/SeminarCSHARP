4827f0271a6aa04dc720e349d4149a3f181d7d61
sergeyt <sergej.tokarev@gmail.com>
2022-06-07 16:46:39 +0300

    Changes saved, Homework folder added

---
+++ Homework_I/Program.cs
@@ -0,0 +1,86 @@
+﻿/* int a, b;
+
+Console.Write("Input integer a: ");
+
+a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input integer b: ");
+
+b = Convert.ToInt32(Console.ReadLine());
+
+if (a == b)
+{
+    Console.WriteLine("No MAX, numbers are equal");
+}
+else if (a > b)
+{
+    Console.WriteLine($"Max = {a}");
+    Console.WriteLine($"Min = {b}");
+}
+else
+{
+    Console.WriteLine($"Max = {b}");
+    Console.WriteLine($"Min = {a}");
+}
+
+*/ 
+/*int a, b, c, max;
+
+Console.Write("Input integer a: ");
+
+a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input integer b: ");
+
+b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input integer c: ");
+
+c = Convert.ToInt32(Console.ReadLine());
+
+
+max = a;
+if (b > a)
+{
+    max = b;
+    
+}
+if (c > max)
+{
+    max = c;
+}
+
+Console.WriteLine($"Max is {max}"); */
+
+/*int number;
+
+Console.Write("Input integer : ");
+number = Convert.ToInt32(Console.ReadLine());
+
+if ((number % 2) == 0)
+{
+    Console.WriteLine($"{number} is even");
+}
+else
+    Console.WriteLine($"{number} is odd");
+
+*/
+
+/*int N;
+
+Console.Write("Input integer number: ");
+N = Convert.ToInt32(Console.ReadLine());
+
+int current = 2;
+
+if (N < current)
+{
+    Console.WriteLine($"{N} is too small");
+}
+
+
+while(current <= N)
+{
+    Console.Write(current + " ");
+    current = current + 2;
+}*/
