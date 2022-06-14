053c5c1651a3cc8232c22e669d076fade2c74755
sergeyt <sergej.tokarev@gmail.com>
2022-06-14 20:06:19 +0300

    Seminar files update, Homework_III folder added

---
+++ Homework_III/Program.cs
@@ -0,0 +1,127 @@
+﻿//ZADACHA 1 VARIANT 1
+
+/*
+Console.WriteLine("Input 5-digit number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+
+void PalindromeFunc(int num)
+{
+    if (num<0)
+    {
+        num = num*-1;
+    }
+    if (num < 10000 || num > 99999)
+    {
+        Console.WriteLine("Given number is not 5-digit");
+    }
+    else
+    {
+    int[] array = num.ToString().ToCharArray().Select(x => x - '0').ToArray();
+    int num1 = array[0];
+    int num2 = array[1];
+    int num4 = array[3];
+    int num5 = array[4];
+    if (num1 != num5 && num2 != num4)
+        Console.WriteLine("Given number is not a palindrome");
+    if (((num > 9999 && num < 100000) && (num1 == num5 && num2 == num4)))
+    {
+        Console.WriteLine("Given number is a palindrome");
+    }
+    }
+}
+
+PalindromeFunc(num);
+*/
+//ZADACHA 1 VARIANT 2
+/*
+Console.WriteLine("Input 5-digit number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+void PalindromeFunc(int num)
+{
+    if (num<0)
+    {
+        num = num*-1;
+    }
+    if (num < 10000 || num > 99999)
+    {
+        Console.WriteLine("Number is not 5-digit");
+    }
+    else
+    {
+        int num1 = num / 10000;
+        int num2 = (num % 10000)/1000;
+        int num3 = (num / 10) % 10;
+        int num4 = num % 10;
+        if (num1 == num4 && num2 == num3)
+        {
+            Console.WriteLine("Given number is a palindrome");
+        }
+        else
+        {
+            Console.WriteLine("Given number is not a palindrome");
+        }
+    }
+}
+PalindromeFunc(num);
+
+*/
+
+
+//ZADACHA 2
+/*
+Console.WriteLine("Input xA");
+double xA = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input yA");
+double yA = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input zA");
+double zA = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input xB");
+double xB = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input yB");
+double yB = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input zB");
+double zB = Convert.ToDouble(Console.ReadLine());
+
+
+double FindLength(double xA, double yA, double zA, double xB, double yB, double zB)
+
+    {
+       return Math.Round(Math.Sqrt((xB-xA)*(xB-xA) + Math.Pow((yB-yA),2) + Math.Pow((zB-zA),2)), 2);
+       
+    }
+  
+FindLength(xA, yA, zA, xB, yB, zB);
+
+Console.WriteLine(FindLength(xA, yA, zA, xB, yB, zB));
+*/
+
+//ZADACHA 3
+/*
+Console.WriteLine("Input number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void CubeTable(int num)
+    {   
+        if (num < 0)
+        {
+        Console.Write("Number < 0, input positive number");    
+        }
+        else
+        {
+            int current = 1;
+            int cube = 1;
+            Console.Write(current);
+            while (current < num)
+            {   
+                current = current+1;
+                cube = current*current*current;
+                Console.Write($" {cube}");
+            }
+        }
+    }
+CubeTable(num);
+*/
---
+++ III/Program.cs
@@ -0,0 +1,71 @@
+﻿/*void FindQuart(int x, int y)
+
+{
+    if (x > 0 && y > 0) return 1;
+    if (x < 0 && y > 0) return 2;
+    if (x < 0 && y < 0) return 3;
+    if (x > 0 && y < 0) return 4;
+
+    return -1;
+}
+
+int result = FindQuart(-2,4);
+
+if (result == -1)
+{
+    Console.WriteLine("Given point is set on axis");
+
+}
+else Console.WriteLine($"Point is in {result} quorter");
+*/
+
+
+/*
+void FindDiapason(int q)
+{
+    if (q >=1 && q<=4)
+    {
+        if (q == 1 ) Console.WriteLine("x and y are positive");
+        if (q == 2 ) Console.WriteLine("x negative and y positive");
+        if (q == 3 ) Console.WriteLine("x negative and y negative");
+        if (q == 4 ) Console.WriteLine("x positive and y negative");
+    }
+    else
+    Console.WriteLine("q is not a quarter number");
+}
+
+Console.WriteLine("Input quarter");
+int q = Convert.ToInt32(Console.ReadLine());
+FindDiapason(q);
+*/
+
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+
+/*
+Console.WriteLine("Input number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void QuadTable(int num)
+    {   
+        
+        int current = 1;
+        int quad = 1;
+        Console.Write(current);
+        while (current < num)
+        {   
+            
+            current = current+1;
+            quad = current*current;
+            Console.Write($" {quad}");
+        }
+        
+    }
+QuadTable(num);
+*/
+
+/*
+double FindLength(double xA, double yA, double xB, double yB);
+{
+    Math.Sqrt((xB-xA)*(xB-xA)+ Math.Pow((yB-yA),2));
+}
+*/
