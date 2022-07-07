b2b7d9c9f5efc2f31076400e579dedb7c0885f5c
sergeyt <sergej.tokarev@gmail.com>
2022-07-07 15:16:53 +0300

    Homework_X folder updated

--- Homework_X/Program.cs
+++ Homework_X/Program.cs
@@ -2,6 +2,48 @@
 
 Пример: { "qwe", "wer", "ert", "rty", "tyu"} -> 1
 */
+
+string[] CreateStringArray(int size )
+{
+    string[] words = new string [size];
+
+    for(int i = 0; i < size; i++)
+    {
+        Console.Write($"Input {i + 1} word: ");
+        words [i]= Console.ReadLine();
+    }
+
+    return words;
+}
+void ShowArray(string[] array)
+{
+   for(int i = 0; i < array.Length; i++)
+      Console.Write(array[i] + " ");
+    
+    Console.WriteLine();
+}
+
+int SymbolsOfWords(string[] array, char a)
+{
+    int count = 0;
+    char[] vowel = new char[] {"a", "e", "i", "o", "u", "y"};
+     for(int i = 0; i < array.Length; i++)
+         if (array[i][0] = vowel[0])
+            count ++;
+            vowel ++;
+
+    return count;        
+}
+Console.Write("Input the size of arrays: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+
+string[] names = CreateStringArray(size);
+char a = 'a'; 
+Console.WriteLine(" " + SymbolsOfWords(names, a));
+
+
+
 /*
 Задача 2: Задайте массив строк. Напишите программу, которая генерирует 
 новый массив, объединяя элементы исходного массива попарно.
