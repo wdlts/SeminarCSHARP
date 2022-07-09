2b1359fd9900bbc2f9717ad1c5e83b50ad02798c
wdltsNB <sergej.tokarev@gmail.com>
2022-07-09 18:55:23 +0300

    Homewor_X folder updated

--- Homework_X/Program.cs
+++ Homework_X/Program.cs
@@ -2,7 +2,7 @@
 
 Пример: { "qwe", "wer", "ert", "rty", "tyu"} -> 1
 */
-
+/*
 string[] CreateStringArray(int size )
 {
     string[] words = new string [size];
@@ -23,25 +23,24 @@ void ShowArray(string[] array)
     Console.WriteLine();
 }
 
-int SymbolsOfWords(string[] array, char a)
+int SymbolsOfWords(string[] array)
 {
     int count = 0;
-    char[] vowel = new char[] {"a", "e", "i", "o", "u", "y"};
+    
      for(int i = 0; i < array.Length; i++)
-         if (array[i][0] = vowel[0])
+         if (array[i][0] == 'a' || array[i][0] == 'e' || array[i][0] == 'i' || array[i][0] == 'o' || array[i][0] == 'u' || array[i][0] == 'y')
             count ++;
-            vowel ++;
+            
 
     return count;        
 }
-Console.Write("Input the size of arrays: ");
+Console.Write("Input the size of array: ");
 int size = Convert.ToInt32(Console.ReadLine());
 
 
 string[] names = CreateStringArray(size);
-char a = 'a'; 
-Console.WriteLine(" " + SymbolsOfWords(names, a));
-
+Console.WriteLine(" " + SymbolsOfWords(names));
+*/
 
 
 /*
@@ -50,3 +49,40 @@ Console.WriteLine(" " + SymbolsOfWords(names, a));
 
 Пример: { "qwe", "wer", "ert", "rty", "tyu", "yui"} -> { "qwewer", "ertrty", "tyuyui"}
 */
+string[] CreateStringArray(int size)
+{
+    string[] words = new string [size];
+
+    for(int i = 0; i < size; i++)
+    {
+        Console.Write($"Input {i + 1} word: ");
+        words [i]= Console.ReadLine();
+    }
+
+    return words;
+}
+
+string[] MatchTwoArrays(int size, string[] array1)
+{
+    string[] words = new string [size];
+
+    for(int i = 0; i < size; i=i+2)
+    {
+        words [i]= array1[i] + "" + array1[i+1];
+    }
+
+    return words;
+}
+
+void ShowArray(string[] array)
+{
+   for(int i = 0; i < array.Length; i++)
+      Console.Write(array[i] + " ");
+    
+    Console.WriteLine();
+}
+
+Console.Write("Input the size of arrays: ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] myarray1 = CreateStringArray(size);
+ShowArray(MatchTwoArrays(size, myarray1));
