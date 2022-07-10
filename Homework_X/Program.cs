55d5a101e4c86798b16403a7dae0da9c332daa07
sergeyt <sergej.tokarev@gmail.com>
2022-07-10 12:57:46 +0300

    Homework_X folder updated

--- Homework_X/Program.cs
+++ Homework_X/Program.cs
@@ -6,40 +6,27 @@
 string[] CreateStringArray(int size )
 {
     string[] words = new string [size];
-
     for(int i = 0; i < size; i++)
     {
         Console.Write($"Input {i + 1} word: ");
-        words [i]= Console.ReadLine();
+        words [i] = Console.ReadLine();
     }
-
     return words;
 }
-void ShowArray(string[] array)
-{
-   for(int i = 0; i < array.Length; i++)
-      Console.Write(array[i] + " ");
-    
-    Console.WriteLine();
-}
 
-int SymbolsOfWords(string[] array)
+int VowelWords(string[] array)
 {
     int count = 0;
-    
-     for(int i = 0; i < array.Length; i++)
-         if (array[i][0] == 'a' || array[i][0] == 'e' || array[i][0] == 'i' || array[i][0] == 'o' || array[i][0] == 'u' || array[i][0] == 'y')
+    for(int i = 0; i < array.Length; i++)
+         if (array[i][0] == 'a' || array[i][0] == 'e' || array[i][0] == 'i' || 
+            array[i][0] == 'o' || array[i][0] == 'u' || array[i][0] == 'y')
             count ++;
-            
-
     return count;        
 }
-Console.Write("Input the size of array: ");
+Console.Write("Input array size: ");
 int size = Convert.ToInt32(Console.ReadLine());
-
-
 string[] names = CreateStringArray(size);
-Console.WriteLine(" " + SymbolsOfWords(names));
+Console.WriteLine("Number of words starting with vowel is " + VowelWords(names));
 */
 
 
@@ -49,40 +36,47 @@ Console.WriteLine(" " + SymbolsOfWords(names));
 
 Пример: { "qwe", "wer", "ert", "rty", "tyu", "yui"} -> { "qwewer", "ertrty", "tyuyui"}
 */
+/*
 string[] CreateStringArray(int size)
 {
     string[] words = new string [size];
-
     for(int i = 0; i < size; i++)
     {
         Console.Write($"Input {i + 1} word: ");
-        words [i]= Console.ReadLine();
+        words [i] = Console.ReadLine();
     }
-
     return words;
 }
 
-string[] MatchTwoArrays(int size, string[] array1)
+string[] MatchArrayWords(int size, string[] array1)
 {
     string[] words = new string [size];
-
     for(int i = 0; i < size; i=i+2)
     {
         words [i]= array1[i] + "" + array1[i+1];
     }
-
     return words;
 }
 
 void ShowArray(string[] array)
 {
-   for(int i = 0; i < array.Length; i++)
+    for(int i = 0; i < array.Length; i++)
       Console.Write(array[i] + " ");
-    
+   
     Console.WriteLine();
 }
 
-Console.Write("Input the size of arrays: ");
+Console.Write("Input array size. Size should be even: ");
 int size = Convert.ToInt32(Console.ReadLine());
-string[] myarray1 = CreateStringArray(size);
-ShowArray(MatchTwoArrays(size, myarray1));
+if (size % 2 !=0)
+{
+    Console.WriteLine("Arrays size should be even.");
+    
+}
+else
+{
+    string[] myarray1 = CreateStringArray(size);
+    ShowArray(MatchArrayWords(size, myarray1));
+}
+
+*/
